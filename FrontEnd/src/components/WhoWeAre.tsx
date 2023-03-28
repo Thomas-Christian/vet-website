016a7079fd3ba8387c680cd61d0efca653ddaaae
Tom Christian <103541283+Thomas-Christian@users.noreply.github.com>
2023-03-28 15:31:19 -0400

    changed how 'board' is rendered on home page

--- FrontEnd/src/components/WhoWeAre.tsx
+++ FrontEnd/src/components/WhoWeAre.tsx
@@ -1,35 +1,51 @@
 import { useState } from "react";
 
+const team = [
+    {
+        "name": "Cathy Christian",
+        "image" : "{require('../images/poop-emoji.jpg')}",
+        "about" : "Cathy was an air force vet, she worked 1000 years in PM @ Prudential, she has a son who is really smart and also attractive."
+    },
+
+    {
+        "name": "Not Cathy Christian",
+        "image" : "{require('../images/poop-emoji.jpg')}",
+        "about" : "Cathy was an air force vet, she worked 1000 years in PM @ Prudential, she has a son who is really smart and also attractive."
+    }
+]
+
 
 export default function Who() {
 
-    const Person = () => {
-
+    const Person = ({person}: any ) => {
+        
         const [visible, setVisible] = useState(false);
 
-        return (
-
-            <div className="text-center items-center justify-center p-4" onClick={() => setVisible(!visible)}> 
-
-                <img alt="" id="pic" src={require('../images/poop-emoji.jpg')} className="rounded-full inline-block w-48 origin-top m-1" />
-
-                <h1 className="font-bold"> Cathy Christian </h1> 
-                
-                <div className={"w-fit flex-wrap " + (visible ? 'flex' : 'hidden') }>
-                    <p> Cathy was an air force vet, she worked 1000 years in PM @ Prudential, she has a son who is really smart and also attractive. </p>
+            return (
+                <div className="text-center items-center justify-center p-4" onClick={() => setVisible(!visible)}> 
+    
+                    <img alt="" id="pic" src={require('../images/poop-emoji.jpg')} className="rounded-full inline-block w-48 origin-top m-1" />
+    
+                    <h1 className="font-bold"> {person.name} </h1> 
+                    
+                    <div className={"w-48 flex-wrap " + (visible ? 'flex' : 'hidden') }>
+                        <p> {person.about} </p>
+                    </div>
+                    
                 </div>
-                
-            </div>
-
-        )
+            )
+        
     }
+    
     
     return (
 
         <div id="Board" className="flex flex-row justify-center p-4 bg-slate-300 w-[80%] relative top-4">
 
-            <Person />
-            <Person /> 
+            {team.map((item) => (
+              <Person person={item} key={item.name} />
+            ))
+            }
             
         </div>
       );
